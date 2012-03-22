f19db79586b0333cf3f507245aa5bfb591080ed4
campbel <none@none.com>
2012-03-22 00:33:24 +0000

    Added Tests for RBF Solver, added a static set of hard puzzles that can be used.

---
+++ Sudoku/Test.2010/HardPuzzles.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokuLibrary.Test
+{
+    /// <summary>
+    /// Puzzles pulled from the "Exceptionally difficult Sudokus (hardest Sudokus)" section
+    /// on the Sudoku Algorithms wikipedia page http://en.wikipedia.org/wiki/Sudoku_algorithms
+    /// </summary>
+    public static class HardPuzzles
+    {
+        //Rating Program: gsf's sudoku q1
+        //Rating: 99529
+        //Poster: eleven
+        //Label: HardestSudokusThread-02085;Discrepancy
+        public static byte[] HardestSudokusThread_02085 = 
+        {
+            1,2,0,4,0,0,3,0,0,
+            3,0,0,0,1,0,0,5,0,
+            0,0,6,0,0,0,1,0,0,
+            7,0,0,0,9,0,0,0,0,
+            0,4,0,6,0,3,0,0,0,
+            0,0,3,0,0,2,0,0,0,
+            5,0,0,0,8,0,7,0,0,
+            0,0,7,0,0,0,0,0,5,
+            0,0,0,0,0,0,0,9,8
+        };
+
+        //Rating Program: Nicolas Juillerat's Sudoku explainer 1.2.1
+        //Rating: 11.9  (ER/EP/ED=11.9/11.9/11.3)
+        //Poster: tarek
+        //Label: golden nugget
+        public static byte[] Golden_Nugget = 
+        {
+            0,0,0,0,0,0,0,3,9,
+            0,0,0,0,0,1,0,0,5,
+            0,0,3,0,5,0,8,0,0,
+            0,0,8,0,9,0,0,0,6,
+            0,7,0,0,0,2,0,0,0,
+            1,0,0,4,0,0,0,0,0,
+            0,0,9,0,8,0,0,5,0,
+            0,2,0,0,0,0,6,0,0,
+            4,0,0,7,0,0,0,0,0
+        };
+
+        //Rating Program: gsf's sudoku q2
+        //Rating: 99743
+        //Poster: eleven
+        //Label: HardestSudokusThread-00245;Red_Dwarf
+        public static byte[] HardestSudokusThread_00245 = 
+        {
+            1,2,0,3,0,0,0,0,4,
+            3,5,0,0,0,0,1,0,0,
+            0,0,4,0,0,0,0,0,0,
+            0,0,5,4,0,0,2,0,0,
+            6,0,0,0,7,0,0,0,0,
+            0,0,0,0,0,8,0,9,0,
+            0,0,3,1,0,0,5,0,0,
+            0,0,0,0,0,9,0,7,0,
+            0,0,0,0,6,0,0,0,8
+        };
+
+        //Rating Program: dukuso's suexrat9
+        //Rating: 10364
+        //Poster: eleven
+        //Label: HardestSudokusThread-01418;coloin
+        public static byte[] HardestSudokusThread_01418 = 
+        {
+            0,0,3,0,0,0,0,0,0,
+            4,0,0,0,8,0,0,3,6,
+            0,0,8,0,0,0,1,0,0,
+            0,4,0,0,6,0,0,7,3,
+            0,0,0,9,0,0,0,0,0,
+            0,0,0,0,0,2,0,0,5,
+            0,0,4,0,7,0,0,6,8,
+            6,0,0,0,0,0,0,0,0,
+            7,0,0,6,0,0,5,0,0
+        };
+    }
+}
---
+++ Sudoku/Test.2010/RecursiveBruteForceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuLibrary.RecursiveBruteForce;
+using System.Diagnostics;
+
+namespace SudokuLibrary.Test
+{
+    /// <summary>
+    /// Test runs of the solution for 
+    /// </summary>
+    [TestClass]
+    public class RecursiveBruteForceTests
+    {
+        [TestMethod]
+        public void HardestSudokusThread_02085()
+        {
+            SudokuBoard board = new SudokuBoard(HardPuzzles.HardestSudokusThread_02085);
+            SudokuBoard solvedBoard = Sudoku.Solve(board);
+            Assert.IsTrue(solvedBoard.IsSolved);
+        }
+
+        [TestMethod]
+        public void Golden_Nugget()
+        {
+            SudokuBoard board = new SudokuBoard(HardPuzzles.Golden_Nugget);
+            SudokuBoard solvedBoard = Sudoku.Solve(board);
+            Assert.IsTrue(solvedBoard.IsSolved);
+        }
+
+        [TestMethod]
+        public void HardestSudokusThread_00245()
+        {
+            SudokuBoard board = new SudokuBoard(HardPuzzles.HardestSudokusThread_00245);
+            SudokuBoard solvedBoard = Sudoku.Solve(board);
+            Assert.IsTrue(solvedBoard.IsSolved);
+        }
+
+        [TestMethod]
+        public void HardestSudokusThread_01418()
+        {
+            SudokuBoard board = new SudokuBoard(HardPuzzles.HardestSudokusThread_01418);
+            SudokuBoard solvedBoard = Sudoku.Solve(board);
+            Assert.IsTrue(solvedBoard.IsSolved);
+        }
+    }
+}
